3ddf5a8d31abdbe4747adca40d090cc8e2b13cd6
Pavan Kumar M G <pa1kumar3324@gmail.com>
2026-03-02 14:14:45 +0530

    Delete sharktank-frontend directory

--- sharktank-frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="h-screen flex items-center justify-center bg-blue-200">
-      <h1 className="text-4xl font-bold text-white">SharkTank testin api!</h1>
-    </div>
-  );
-}                        
